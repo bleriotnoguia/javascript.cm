56c01df920f1f8f391febb710a9a379f8e23fb89
Bleriot Noguia <nstevebleriot@yahoo.fr>
2025-01-14 09:26:10 +0100

    ✨ Refactor users table migration: Update schema to include username, avatar, and social media IDs; enhance timestamps and default values for admin and sponsor roles.

--- database/migrations/1736839410875_create_users_table.ts
+++ database/migrations/1736839410875_create_users_table.ts
@@ -5,13 +5,19 @@ export default class extends BaseSchema {
 
   async up() {
     this.schema.createTable(this.tableName, (table) => {
-      table.increments('id').notNullable()
-      table.string('full_name').nullable()
-      table.string('email', 254).notNullable().unique()
+      table.increments('id').primary()
+      table.string('username').unique().notNullable()
+      table.string('name').nullable()
+      table.string('email').unique().notNullable()
       table.string('password').notNullable()
-
-      table.timestamp('created_at').notNullable()
-      table.timestamp('updated_at').nullable()
+      table.string('avatar').nullable()
+      table.string('github_id').unique().nullable()
+      table.string('twitter_id').unique().nullable()
+      table.boolean('is_admin').defaultTo(false)
+      table.boolean('is_sponsor').defaultTo(false)
+      table.timestamp('email_verified_at').nullable()
+      table.string('remember_me_token').nullable()
+      table.timestamps(true, true)
     })
   }
 
